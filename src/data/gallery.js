ae6364a2b09c041d3953455a60b3a85e41c79efc
Kyal-Sin-Phyo <ksp1612006@gmail.com>
2026-07-08 20:29:54 +0630

    Create Product Gallery File

---
+++ src/data/gallery.js
@@ -0,0 +1,61 @@
+
+import headphoneABlack from "./../assets/images/headphone-a/Headphone-a-black.webp";
+import headphoneAWhite from "./../assets/images/headphone-a/Headphone-a-white.webp";
+import headphoneAYellow from "./../assets/images/headphone-a/Headphone-a-yellow.webp";
+import headphoneAPink from "./../assets/images/headphone-a/Headphone-a-pink.webp";
+
+import headphone1Black from "./../assets/images/headphone-1/Headphone1-black.webp";
+import headphone1White from "./../assets/images/headphone-1/Headphone1-white.webp";
+
+import cmfHeadphoneProDarkGrey from "./../assets/images/cmf-headphone-pro/CMF_Headphone-Dark-Grey.webp";
+import cmfHeadphoneProLightGreen from "./../assets/images/cmf-headphone-pro/CMF_Headphone-Light-Green.webp";
+import cmfHeadphoneProLightGrey from "./../assets/images/cmf-headphone-pro/CMF_Headphone-Light-Grey.webp";
+
+
+const gallery = [
+    {
+        id: 1,
+        name: "Headphone (A)",
+        image: headphoneABlack,
+    },
+    {
+        id: 2,
+        name: "Headphone (A)",
+        image: headphoneAWhite,
+    },
+    {
+        id: 3,
+        name: "Headphone (A)",
+        image: headphoneAYellow,
+    },
+    {
+        id: 4,
+        name: "Headphone (A)",
+        image: headphoneAPink,
+    },
+    {
+        id: 5,
+        name: "Headphone (1)",
+        image: headphone1Black,
+    },
+    {
+        id: 6,
+        name: "Headphone (1)",
+        image: headphone1White,
+    },
+    {
+        id: 7,
+        name: "CMF Headphone Pro",
+        image: cmfHeadphoneProDarkGrey,
+    },
+    {
+        id: 8,
+        name: "CMF Headphone Pro",
+        image: cmfHeadphoneProLightGreen,
+    },
+    {
+        id: 9,
+        name: "CMF Headphone Pro",
+        image: cmfHeadphoneProLightGrey,
+    },
+]
