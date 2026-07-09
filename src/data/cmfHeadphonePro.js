9005d82f125ac4d3cd852a05d6290ad31441587b
Kyal-Sin-Phyo <ksp1612006@gmail.com>
2026-07-10 00:09:30 +0630

    fix: final rename product image file

--- src/data/cmfHeadphonePro.js
+++ src/data/cmfHeadphonePro.js
@@ -1,6 +1,6 @@
 import cmfHeadphoneProDarkGrey from "./../assets/images/cmf-headphone-pro/CMF_Headphone-Dark-Grey.webp";
 import cmfHeadphoneProLightGreen from "./../assets/images/cmf-headphone-pro/CMF_Headphone-Light-Green.webp";
-import cmfHeadphoneProLightGrey from "./../assets/images/cmf-headphone-pro/CMF_Headphone-Light-grey.webp";
+import cmfHeadphoneProLightGrey from "./../assets/images/cmf-headphone-pro/CMF_Headphone-Light-Grey.webp";
 
 export const cmfHeadphonePro = {
     id: 2,
